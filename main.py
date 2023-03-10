1fe106d75486578c0ea9edfd64a1b6eaab15f30a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-10 09:55:30 +0300

    Created the dummy endpoint function

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 """The entrypoint of the project"""
+from fastapi import FastAPI
+from todo import todo_router
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def dummy_endpoint() -> dict:
+    """The dummy root endpoint
+
+    Returns:
+        dict: The returned object
+    """
+    return {"Hello": "Good Morning"}
+
+app.include_router(todo_router)
