d5e460ab9b4988fc787bf3828f2420c0f6b6ebda
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-10 10:41:28 +0300

    Defined teh delete todo endpoint

--- todo.py
+++ todo.py
@@ -72,3 +72,28 @@ async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The I
     return {
         "Message": "Todo with supplied Id doesn't exist"
     }
+
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+    """The delete Todo endpoint
+
+    Args:
+        todo_id (int): The id of the todo
+
+    Returns:
+        dict: A message
+    """
+    for index in range(len(todo_list)):
+        todo == todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            todo_list.pop(index)
+
+            return {
+                "Message": "Todo deleted successfully"
+            }
+
+    return {
+        "Message": "Todo deleted successfully"
+    }
