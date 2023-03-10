0754dccbf0bbd98cd91f356fc95b682fb9aaea35
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-10 09:50:48 +0300

    Added docstrings to the two endpoint functions in the todo python script

--- todo.py
+++ todo.py
@@ -9,10 +9,23 @@ todo_list = []
 
 @todo_router.post("/todo")
 async def add_todo(todo: dict) -> dict:
+    """Dumb endpoint for adding todos
+
+    Args:
+        todo (dict): The todo data structure
+
+    Returns:
+        dict: The data structure of the returned value
+    """
     todo_list.append(todo)
     return {"Message": "Todo added successfully"}
 
 
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
+    """A dummy endpoints for fetching todos
+
+    Returns:
+        dict: The returned data structure
+    """
     return {"Todos": todo_list}
